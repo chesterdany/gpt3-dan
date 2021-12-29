56d3e8105cab75ad736598d2de8d1cff8da1e665
Brendea Daniel <chesterdany22@yahoo.com>
2021-12-29 15:54:47 +0200

    feature and whatgpt3 done

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,16 +5,16 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="whpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title="What is GPT-3" text="Lorem ipsum dolor sit amet consectetur adipisicing." />
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
         <p>Explore the library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title="Chatbots" text="Lorem ipsum dolor sit amet consectetur adipisicing." />
+        <Feature title="Knowledgebase" text="Lorem ipsum dolor sit amet consectetur adipisicing." />
+        <Feature title="Education" text="Lorem ipsum dolor sit amet consectetur adipisicing." />
       </div>
     </div>
   );
