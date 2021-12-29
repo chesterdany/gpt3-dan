fbdb2a642f185f0a64c65fd6ff32a0d815164b0b
Brendea Daniel <chesterdany22@yahoo.com>
2021-12-29 14:56:20 +0200

    imports added,brand finished,whatgpt3 basic

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
-        <h1 className="gradient__text">Let't Build Something amazing with GPT-3 OpenAI</h1>
+        <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, deserunt?Lorem ipsum
           dolor sit amet consectetur adipisicing elit. Repellat, deserunt?
