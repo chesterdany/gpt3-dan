8e9c5db1e3f951c652628966172d7c2b69dc9a96
Brendea Daniel <chesterdany22@yahoo.com>
2021-12-29 16:27:25 +0200

    features addded

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,7 +1,41 @@
+import { Feature } from "../../components";
 import "./features.css";
 
+const featuresData = [
+  {
+    title: "Improving end distusts instantly",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, eaque.",
+  },
+  {
+    title: "Become the tended active",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, eaque.",
+  },
+  {
+    title: "Message or am nothing",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, eaque.",
+  },
+  {
+    title: "Really boy law county",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, eaque.",
+  },
+];
+
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It.Step into Future Today & Make it Happen.
+        </h1>
+        <p>Request early access</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresData.map(({ title, text }, index) => (
+          <Feature key={index + title} title={title} text={text} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Features;
