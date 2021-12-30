7486914dfb732229d0829a6a6274b2e3f45529f3
Brendea Daniel <chesterdany22@yahoo.com>
2021-12-30 13:54:52 +0200

    possibility added

--- src/containers/posibility/Posibility.jsx
+++ src/containers/posibility/Posibility.jsx
@@ -1,7 +1,23 @@
 import "./posibility.css";
+import possibilityImage from "../../assets/possibility.png";
 
 const Posibility = () => {
-  return <div>Posibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="posibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error recusandae necessitatibus
+          alias quos nesciunt? Saepe?
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Posibility;
